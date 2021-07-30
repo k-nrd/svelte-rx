92b1445cd6dcee4214a16bb3ad98feef389f0576
Gustavo Konrad <g.konrad@outlook.com>
2021-07-30 11:48:21 -0300

    also copy components to dist

---
+++ dist/components/Connect.svelte
@@ -0,0 +1,32 @@
+<script>
+  import { combineLatest } from "rxjs";
+  import { map, mapTo, distinctUntilChanged } from "rxjs/operators";
+  import { useStore } from "../index";
+
+  // { todos: state => state.todos }
+  export let selectors = {};
+  // { createTodo: (payload) => ({ type: 'CREATE_TODO', payload }) }
+  export let actions = {};
+
+  const stateToProps = ss => model =>
+    Object.keys(ss).reduce((acc, cur) => ({ ...acc, [cur]: ss[cur](model) }), {});
+
+  const dispatchToProps = as => dispatch =>
+    Object.keys(as).reduce(
+      (acc, cur) => ({ ...acc, [cur]: pd => dispatch(as[cur](pd)) }),
+      {}
+    );
+
+  const [state$, dispatch] = useStore();
+
+  const selectors$ = state$.pipe(map(stateToProps(selectors)));
+
+  const actions$ = state$.pipe(mapTo(dispatchToProps(actions)(dispatch)));
+
+  const props$ = combineLatest(selectors$, actions$).pipe(
+    map(([ss, as]) => ({ ...ss, ...as })),
+    distinctUntilChanged()
+  );
+</script>
+
+<slot props={$props$} propsObservable={props$}></slot>
